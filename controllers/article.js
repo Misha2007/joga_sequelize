96c1bb4efccfa84b0620d34ba856eabbc4d003a1
Misha2007 <mishadrogovoz@gmail.com>
2025-02-12 09:10:26 +0200

    Getted all articles data and getted article data by slug

---
+++ controllers/article.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  "mysql://root:qwerty@localhost:3306/joga_sequelize"
+);
+
+const Article = require("../models/article")(sequelize, Sequelize.DataTypes);
+
+const getAllArticles = (req, res) => {
+  Article.findAll()
+    .then((articles) => {
+      res.status(200).json({ articles });
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
+};
+
+const getArticleBySlug = (req, res) => {
+  Article.findOne({
+    where: { slug: req.params.slug },
+  })
+    .then((article) => {
+      console.log(article);
+      res.status(200).json({ article });
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
+};
+
+module.exports = { getAllArticles, getArticleBySlug };
--- index.js
+++ index.js
@@ -4,23 +4,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  "mysql://root:qwerty@localhost:3306/joga_sequelize"
-);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connected to the database");
-  })
-  .catch((err) => {
-    console.error("Error connecting to the database:", err);
-  });
-
-app.get("/", function (req, res) {
-  res.json({ message: "Welcome to sequelize application" });
-});
+const articleRouter = require("./routes/article");
+app.use("/", articleRouter);
+app.use("/article", articleRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port http://localhost:3000");
